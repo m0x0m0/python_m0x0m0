d8478093e67e37f060551967f49ebd9f51bf704b
Sophia_Evdokimova <evdokimovasofa0@gmail.com>
2025-02-02 16:12:34 +0700

    add

---
+++ ЕГЭ/19-21/3.py
@@ -0,0 +1,21 @@
+'''
++1 *2
+win >=46
+1 <= S <= 45
+'''
+
+def f(s, m):
+    if (m == 2 or m == 4) and s >= 46:
+        return 1
+    elif m == 4 and s < 46:
+        return 0
+    elif m < 4 and s >= 46:
+        return 0
+    else:
+        if m % 2 == 0:
+            return f(s + 1, m + 1) and f(s * 2, m + 1)
+        else:
+            return f(s + 1, m + 1) or f(s * 2, m + 1)
+for i in range(1, 46):
+    if f(i, 0):
+        print(i)
